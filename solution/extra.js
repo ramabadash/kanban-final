d99723e28338bd03067d19b54b570926fd9ac264
ramabadash <ramabadash@gmail.com>
2021-09-25 22:39:33 +0300

    # JS - Fixed clock on top (extra)
    The function takes the current time and displays it at the top of the page, it calls itself every second and thus is updated regularly.

--- solution/extra.js
+++ solution/extra.js
@@ -43,3 +43,19 @@ function goodJob() {
     imgElem.style.display = "flex"; //show img
     setTimeout(() => imgElem.style.display = "none", 2000); //hidde img
 }
+//Fixed clock
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').textContent =  `${h} : ${m} : ${s}`;
+    setTimeout(startTime, 1000);
+}
+//  
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
