1e29038accf4fdcd625a59e430f0905e8b5dd965
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-10-11 22:38:15 +0300

    Installing ESLint and prettier and fixing code.
    Setting an Airbnb standard.
    Before fixing problems !  Code Style correction only.

--- solution/assistFunctions.js
+++ solution/assistFunctions.js
@@ -1,145 +1,161 @@
-"use strict"
-
 /*---------------------------------------------------------------*/
-/**********Folder: "SERVICES", File: "DOM.JS" **********/
+/** ********Folder: "SERVICES", File: "DOM.JS" ********* */
 /*---------------------------------------------------------------*/
 
-//play loader
+// play loader
 function playLoader() {
-    const loader = document.querySelector("#loader");
-    loader.classList.add("loader");
-    loader.style.display = "block";
+  const loader = document.querySelector('#loader')
+  loader.classList.add('loader')
+  loader.style.display = 'block'
 }
-//stop loader
+// stop loader
 function stopLoader() {
-    loader.classList.remove("loader");
-    loader.style.display = "none";
+  loader.classList.remove('loader')
+  loader.style.display = 'none'
 }
-//Rebuilds the elements in DOM based on local storage
+// Rebuilds the elements in DOM based on local storage
 function updateDom() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    for (let key in tasks) {
-        let listElem = document.getElementById(key);
-        //If there are existing tasks in memory you will create new elements
-        if (tasks[key].length > 0) {
-            for (let content of tasks[key]) {
-                let newTaskElem = createListElement(content);
-                listElem.appendChild(newTaskElem);
-            }
-        }
+  tasks = JSON.parse(localStorage.getItem('tasks'))
+  for (const key in tasks) {
+    const listElem = document.getElementById(key)
+    // If there are existing tasks in memory you will create new elements
+    if (tasks[key].length > 0) {
+      for (const content of tasks[key]) {
+        const newTaskElem = createListElement(content)
+        listElem.appendChild(newTaskElem)
+      }
     }
+  }
 }
 //
 function cleanDom() {
-    const allTaskElem = document.querySelectorAll(".task");
-    for (let task of allTaskElem) task.remove();
+  const allTaskElem = document.querySelectorAll('.task')
+  for (const task of allTaskElem) task.remove()
 }
-//Create List Element
-function createListElement (innerText) {
-    const eventListeners = {mouseover: changeTaskList, click: editTask, dragstart: draggableDataTransfer};
-    return createElement("li", innerText, [], ["task"], {"draggable": true}, eventListeners);
+// Create List Element
+function createListElement(innerText) {
+  const eventListeners = {
+    mouseover: changeTaskList,
+    click: editTask,
+    dragstart: draggableDataTransfer,
+  }
+  return createElement(
+    'li',
+    innerText,
+    [],
+    ['task'],
+    { draggable: true },
+    eventListeners
+  )
 }
-//Create Element
-function createElement(tagName, innerText, children = [], classes = [], attributes = {}, eventListeners = {}) {
-    let newElement = document.createElement(tagName);
-    //children
-    for (let child of children){
-        newElement.append(child);
-    }
-    //classes
-    for (let clas of classes){
-        newElement.classList.add(clas);
-    }
-    //attributes
-    const attributesKeys = Object.keys(attributes);
-    const attributesValues =Object.values(attributes);
-    for (let i = 0; i < attributesKeys.length; i++){
-        newElement.setAttribute(attributesKeys[i],attributesValues[i]);
-    }
-    //eventListeners
-    const eventListenersKeys = Object.keys(eventListeners);
-    const eventListenersValues =Object.values(eventListeners);
-    for (let i = 0; i < eventListenersKeys.length; i++){
-        newElement.addEventListener(eventListenersKeys[i],eventListenersValues[i]);
-    }
-    //inner text
-    newElement.textContent = innerText;
+// Create Element
+function createElement(
+  tagName,
+  innerText,
+  children = [],
+  classes = [],
+  attributes = {},
+  eventListeners = {}
+) {
+  const newElement = document.createElement(tagName)
+  // children
+  for (const child of children) {
+    newElement.append(child)
+  }
+  // classes
+  for (const clas of classes) {
+    newElement.classList.add(clas)
+  }
+  // attributes
+  const attributesKeys = Object.keys(attributes)
+  const attributesValues = Object.values(attributes)
+  for (let i = 0; i < attributesKeys.length; i++) {
+    newElement.setAttribute(attributesKeys[i], attributesValues[i])
+  }
+  // eventListeners
+  const eventListenersKeys = Object.keys(eventListeners)
+  const eventListenersValues = Object.values(eventListeners)
+  for (let i = 0; i < eventListenersKeys.length; i++) {
+    newElement.addEventListener(eventListenersKeys[i], eventListenersValues[i])
+  }
+  // inner text
+  newElement.textContent = innerText
 
-    return newElement;
+  return newElement
 }
 
 /*---------------------------------------------------------------*/
-/**********Folder: "SERVICES", File: "VADILATOR.JS" **********/
+/** ********Folder: "SERVICES", File: "VADILATOR.JS" ********* */
 /*---------------------------------------------------------------*/
 
-//Takes the information from the local storage, if the information is empty assign it an empty task template. Returns the updated content.
+// Takes the information from the local storage, if the information is empty assign it an empty task template. Returns the updated content.
 function localStorageValidator() {
-    let primaryData = JSON.parse(window.localStorage.getItem('tasks'));  //gets local primary data after refresh
-    if(primaryData === null) { // There is no local storage -> define new empty one
-        tasks = {"todo":[], "in-progress":[], "done":[] };
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    } 
-    return JSON.parse(window.localStorage.getItem('tasks'));
+  const primaryData = JSON.parse(window.localStorage.getItem('tasks')) // gets local primary data after refresh
+  if (primaryData === null) {
+    // There is no local storage -> define new empty one
+    tasks = { todo: [], 'in-progress': [], done: [] }
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }
+  return JSON.parse(window.localStorage.getItem('tasks'))
 }
 
 // If the input is blank it will throw a note to the user and otherwise return the input
 function inputVadilator(input) {
-    //Trying to submit empty tasks
-    if (input.length === 0) { 
-        alert ("Can't insert empty task. Try again");
-        return;
-    } else return input; 
+  // Trying to submit empty tasks
+  if (input.length === 0) {
+    alert("Can't insert empty task. Try again")
+  } else return input
 }
 
 /*---------------------------------------------------------------*/
-/**********Folder: "SERVICES", File: "LOCAL-STORAGE.JS" **********/
+/** ********Folder: "SERVICES", File: "LOCAL-STORAGE.JS" ********* */
 /*---------------------------------------------------------------*/
 
 //
-function saveDataLocal (data) {
-    localStorage.setItem("tasks", JSON.stringify(data));
+function saveDataLocal(data) {
+  localStorage.setItem('tasks', JSON.stringify(data))
 }
-//Save to local all list new information
-function saveNewDataLocal (key, list) {
-    const taskList = list.childNodes;
-    const newTaskArray = [];
-    //create an array of content
-    for (let i = 0; i < taskList.length; i++) { 
-        let taskContent = taskList[i].textContent;
-        newTaskArray.push(taskContent);
-    }
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    tasks[key] = newTaskArray;
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    updateTotal();
-}
-
-/*---------------------------------------------------------------*/
-/**********Folder: "SERVICES", File: "TASKS.JS" **********/
-/*---------------------------------------------------------------*/
-
-//Add task on top
-function taskOnTop (newTaskElem, taskList) {
-    if (!newTaskElem || !taskList) return;
-    if (taskList.id === "done") goodJob();
-    taskList.insertBefore(newTaskElem, taskList.firstElementChild);
+// Save to local all list new information
+function saveNewDataLocal(key, list) {
+  const taskList = list.childNodes
+  const newTaskArray = []
+  // create an array of content
+  for (let i = 0; i < taskList.length; i++) {
+    const taskContent = taskList[i].textContent
+    newTaskArray.push(taskContent)
+  }
+  tasks = JSON.parse(localStorage.getItem('tasks'))
+  tasks[key] = newTaskArray
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+  updateTotal()
 }
 
 /*---------------------------------------------------------------*/
-/**********Folder: "SERVICES", File: "TASKS-LIST.JS" **********/
+/** ********Folder: "SERVICES", File: "TASKS.JS" ********* */
+/*---------------------------------------------------------------*/
+
+// Add task on top
+function taskOnTop(newTaskElem, taskList) {
+  if (!newTaskElem || !taskList) return
+  if (taskList.id === 'done') goodJob()
+  taskList.insertBefore(newTaskElem, taskList.firstElementChild)
+}
+
+/*---------------------------------------------------------------*/
+/** ********Folder: "SERVICES", File: "TASKS-LIST.JS" ********* */
 /*---------------------------------------------------------------*/
 
 //
 function nextListByKey(event) {
-    let nextList;
-    if(event.altKey && event.key === "1"){
-        nextList = document.querySelector(".to-do-tasks");
-    } else if (event.altKey && event.key === "2"){
-        nextList = document.querySelector(".in-progress-tasks");
-    } else if (event.altKey && event.key === "3") {
-        nextList = document.querySelector(".done-tasks");
-    } else {
-        nextList = undefined;
-    }
-    return nextList;
+  let nextList
+  if (event.altKey && event.key === '1') {
+    nextList = document.querySelector('.to-do-tasks')
+  } else if (event.altKey && event.key === '2') {
+    nextList = document.querySelector('.in-progress-tasks')
+  } else if (event.altKey && event.key === '3') {
+    nextList = document.querySelector('.done-tasks')
+  } else {
+    nextList = undefined
+  }
+  return nextList
 }
--- solution/extra.js
+++ solution/extra.js
@@ -1,89 +1,89 @@
-"use strict"
 /*---------------------------------------------------------------*/
-/**********Folder: "SERVICES", File: "DOM.JS" **********/
+/** ********Folder: "SERVICES", File: "DOM.JS" ********* */
 /*---------------------------------------------------------------*/
 
-//clear tasks from DOM and local storage
+// clear tasks from DOM and local storage
 function clearPage() {
-    cleanDom();
-    tasks = {"todo":[], "in-progress":[], "done":[] }; 
-    localStorage.setItem('tasks', JSON.stringify(tasks)); // delete local storage
-    updateTotal();
+  cleanDom()
+  tasks = { todo: [], 'in-progress': [], done: [] }
+  localStorage.setItem('tasks', JSON.stringify(tasks)) // delete local storage
+  updateTotal()
 }
 
 /*---------------------------------------------------------------*/
-/**********Folder: "SERVICES", File: "TIME-INTICATOR.JS" **********/
+/** ********Folder: "SERVICES", File: "TIME-INTICATOR.JS" ********* */
 /*---------------------------------------------------------------*/
 
-//show and hidde opening messege - Depends on the hour 
+// show and hidde opening messege - Depends on the hour
 function openMessage() {
-    const hour = currentHour();
-    let meseegeElem = elemByDaySection(hour);
-    meseegeElem.classList.add("display"); //show messege
-    setTimeout(() => meseegeElem.classList.remove("display"), 3000); //hidde messege
+  const hour = currentHour()
+  const meseegeElem = elemByDaySection(hour)
+  meseegeElem.classList.add('display') // show messege
+  setTimeout(() => meseegeElem.classList.remove('display'), 3000) // hidde messege
 }
 //
-function currentHour(){
-    const splitDate = Date().split(" ");
-    const splitTime = splitDate[4].split(":"); //get time and split to hour, min, sec
-    const hour = splitTime[0];
-    return hour;
+function currentHour() {
+  const splitDate = Date().split(' ')
+  const splitTime = splitDate[4].split(':') // get time and split to hour, min, sec
+  const hour = splitTime[0]
+  return hour
 }
-//Returns the appropriate element to display according to the hour.
+// Returns the appropriate element to display according to the hour.
 function elemByDaySection(hour) {
-    let meseegeElem;
-    if (hour > 6 && hour < 12) {
-        meseegeElem = document.querySelector(".morning");
-    } else if (hour > 12 && hour < 18) {
-        meseegeElem = document.querySelector(".afternoon");
-    } else {
-        meseegeElem = document.querySelector(".night"); 
-    }
-    return meseegeElem;
+  let meseegeElem
+  if (hour > 6 && hour < 12) {
+    meseegeElem = document.querySelector('.morning')
+  } else if (hour > 12 && hour < 18) {
+    meseegeElem = document.querySelector('.afternoon')
+  } else {
+    meseegeElem = document.querySelector('.night')
+  }
+  return meseegeElem
 }
-//Fixed clock
+// Fixed clock
 function startTime() {
-    const today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('clock').textContent =  `${h} : ${m} : ${s}`;
-    setTimeout(startTime, 1000);
+  const today = new Date()
+  const h = today.getHours()
+  let m = today.getMinutes()
+  let s = today.getSeconds()
+  m = checkTime(m)
+  s = checkTime(s)
+  document.getElementById('clock').textContent = `${h} : ${m} : ${s}`
+  setTimeout(startTime, 1000)
 }
 // Add zero in front of numbers < 10
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+  if (i < 10) {
+    i = `0${i}`
+  } // add zero in front of numbers < 10
+  return i
 }
 
-
 /*---------------------------------------------------------------*/
-/**********Folder: "SERVICES", File: "TASKS.JS" **********/
+/** ********Folder: "SERVICES", File: "TASKS.JS" ********* */
 /*---------------------------------------------------------------*/
 
-//Show "good job" img and hidde after 2 seconds
+// Show "good job" img and hidde after 2 seconds
 function goodJob() {
-    const imgElem = document.querySelector("img");
-    imgElem.style.display = "flex"; //show img
-    setTimeout(() => imgElem.style.display = "none", 2000); //hidde img
+  const imgElem = document.querySelector('img')
+  imgElem.style.display = 'flex' // show img
+  setTimeout(() => (imgElem.style.display = 'none'), 2000) // hidde img
 }
 
 /*---------------------------------------------------------------*/
-/**********Folder: "SERVICES", File: "TASKS-LIST.JS" **********/
+/** ********Folder: "SERVICES", File: "TASKS-LIST.JS" ********* */
 /*---------------------------------------------------------------*/
 
-//Updates total tasks based on local storage
+// Updates total tasks based on local storage
 function updateTotal() {
-    const allListsElem = document.querySelectorAll("ul");
-    const data = JSON.parse(localStorage.getItem("tasks"));
-    const allLabel = document.querySelectorAll("label");
-    let i = 0;
-    for (let list of allListsElem) {
-        const totalNum = data[list.id].length; // data[list.id] = tasks[key]
-        const totalTasks = allLabel[i]; //Reaches total according to the selected list
-        totalTasks.textContent = `Total: ${totalNum}`; 
-        i++;
-    }
+  const allListsElem = document.querySelectorAll('ul')
+  const data = JSON.parse(localStorage.getItem('tasks'))
+  const allLabel = document.querySelectorAll('label')
+  let i = 0
+  for (const list of allListsElem) {
+    const totalNum = data[list.id].length // data[list.id] = tasks[key]
+    const totalTasks = allLabel[i] // Reaches total according to the selected list
+    totalTasks.textContent = `Total: ${totalNum}`
+    i++
+  }
 }
